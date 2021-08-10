03ddabbe159f18f7099ae0d226e2a31c87c7a9b4
BohdanOmelianec <omelianecb@gmail.com>
2021-08-10 12:09:10 +0300

    edit router

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App';
 
 
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById('root')
